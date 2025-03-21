12b406077f8e032948f69b2ff5bb488d9266c3c4
Araujo22975 <alex.silveira@iesb.edu.br>
2025-03-20 21:19:57 -0300

    atividade6

--- Projeto-React-Native/Exercicio-04/Flamengo/App.js
+++ Projeto-React-Native/Exercicio-04/Flamengo/App.js
@@ -13,12 +13,16 @@ export default function App() {
 
   return (
     <ScrollView>
+
+ 
+
+
       <View style={styles.container}>
         <StatusBar style="auto" />
-        
+
         <Text style={{ fontSize: 25 }}>{nome}</Text>
-        
-        
+
+
         <Text style={styles.textoGrande}>1 Mundial</Text>
         <Text style={styles.textoGrande}>3 Libertadores</Text>
         <Text style={styles.textoGrande}>39 Cariocas</Text>
@@ -31,7 +35,7 @@ export default function App() {
           source={{
             uri: 'https://folhadoleste.com.br/wp-content/uploads/2025/03/Flamengo-x-Fluminense-Carioca-1024x551.jpg'
           }}
-          
+
           style={{
             height: 225,
             width: 220
@@ -44,7 +48,7 @@ export default function App() {
             width: 260
           }}
 
-        /> <Text style={{fontSize: 25}}>O **Clássico Botafogo x Flamengo** é um dos maiores confrontos do futebol carioca, conhecido também como **Clássico Vovô**. Ele é marcado pela rivalidade histórica entre as duas equipes, com o Flamengo sendo um dos clubes mais vitoriosos do Brasil e o Botafogo, com uma rica tradição e uma torcida fiel. O primeiro confronto aconteceu em 1912, e desde então, os jogos entre eles sempre geram muita emoção e disputas intensas.</Text>
+        /> <Text style={{ fontSize: 25 }}>O **Clássico Botafogo x Flamengo** é um dos maiores confrontos do futebol carioca, conhecido também como **Clássico Vovô**. Ele é marcado pela rivalidade histórica entre as duas equipes, com o Flamengo sendo um dos clubes mais vitoriosos do Brasil e o Botafogo, com uma rica tradição e uma torcida fiel. O primeiro confronto aconteceu em 1912, e desde então, os jogos entre eles sempre geram muita emoção e disputas intensas.</Text>
         <Image
           source={require('./Imagens/Flamengo.png')}
           style={{
@@ -52,7 +56,7 @@ export default function App() {
             width: 260
           }}
 
-        /> <Text style={{fontSize: 25}}>O símbolo do Flamengo é composto pelas cores vermelho e preto, representando a paixão e força do clube. As letras CRF no centro do escudo representam o nome do clube, Clube de Regatas do Flamengo. O design do escudo remete à tradição do clube, que começou como um clube de regatas. A Cruz de Malta no símbolo faz referência às suas origens no remo. O emblema simboliza a maior torcida do Brasil e o status do clube como um dos mais vitoriosos do país.</Text>
+        /> <Text style={{ fontSize: 25 }}>O símbolo do Flamengo é composto pelas cores vermelho e preto, representando a paixão e força do clube. As letras CRF no centro do escudo representam o nome do clube, Clube de Regatas do Flamengo. O design do escudo remete à tradição do clube, que começou como um clube de regatas. A Cruz de Malta no símbolo faz referência às suas origens no remo. O emblema simboliza a maior torcida do Brasil e o status do clube como um dos mais vitoriosos do país.</Text>
 
 
         <Image
@@ -62,7 +66,7 @@ export default function App() {
             width: 260
           }}
 
-        /> <Text style={{fontSize: 25}}>**Gabigol** (Gabriel Barbosa) é um dos maiores ídolos recentes do **Flamengo**. Chegou ao clube em 2019 e se destacou por sua habilidade em marcar gols, especialmente nas finais. Foi o herói da **Libertadores de 2019**, marcando dois gols na final contra o River Plate, incluindo o gol da vitória. Também ajudou o Flamengo a conquistar o **Campeonato Brasileiro de 2019**, além de outros títulos importantes. Sua presença decisiva e carisma o tornaram um ícone da torcida.</Text>
+        /> <Text style={{ fontSize: 25 }}>**Gabigol** (Gabriel Barbosa) é um dos maiores ídolos recentes do **Flamengo**. Chegou ao clube em 2019 e se destacou por sua habilidade em marcar gols, especialmente nas finais. Foi o herói da **Libertadores de 2019**, marcando dois gols na final contra o River Plate, incluindo o gol da vitória. Também ajudou o Flamengo a conquistar o **Campeonato Brasileiro de 2019**, além de outros títulos importantes. Sua presença decisiva e carisma o tornaram um ícone da torcida.</Text>
 
         <Image
           source={require('./Imagens/Imagem.png')}
@@ -71,9 +75,9 @@ export default function App() {
             width: 260
           }}
 
-        /> <Text style={{fontSize: 25}}>A **Flastore** é a loja oficial do **Flamengo**, onde os torcedores podem comprar produtos do clube, como camisas, acessórios, souvenirs, e itens de colecionador. A loja tem tanto versões físicas, localizadas em pontos estratégicos como o **Maracanã**, quanto uma plataforma online para compras pela internet.
+        /> <Text style={{ fontSize: 25 }}>A **Flastore** é a loja oficial do **Flamengo**, onde os torcedores podem comprar produtos do clube, como camisas, acessórios, souvenirs, e itens de colecionador. A loja tem tanto versões físicas, localizadas em pontos estratégicos como o **Maracanã**, quanto uma plataforma online para compras pela internet.
 
-        Além de ser um ponto de venda, a Flastore também representa a paixão da torcida flamenguista, oferecendo produtos oficiais que conectam os torcedores com o time. Se você quiser mais informações sobre a loja ou quiser saber sobre um produto específico, me avise!</Text>
+          Além de ser um ponto de venda, a Flastore também representa a paixão da torcida flamenguista, oferecendo produtos oficiais que conectam os torcedores com o time. Se você quiser mais informações sobre a loja ou quiser saber sobre um produto específico, me avise!</Text>
 
 
 
---
+++ Projeto-React-Native/fundamentos-react-native/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,17 @@
+// Imports
+import React from "react";
+import { Text, View } from 'react-native'
+
+//Função do Componente
+export default function PrimeiroComponente() {
+
+    //Retorno com o JSX
+    return (
+        <View>
+            <Text>
+                Meu Primeiro Componente
+            </Text>
+        </View>
+    )
+
+}
